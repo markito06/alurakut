54f72d20575036c3057adb22cb8d8709263a834f
Marcos Alexandre V. Costa <marcos@substractum.com.br>
2021-07-15 19:21:27 -0300

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,19 +8,19 @@ export default class MyDocument extends Document {
 
     try {
       ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          originalRenderPage({
+            enhanceApp: (App) => (props) =>
+                sheet.collectStyles(<App {...props} />),
+          })
 
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
+            <>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+            </>
         ),
       }
     } finally {
